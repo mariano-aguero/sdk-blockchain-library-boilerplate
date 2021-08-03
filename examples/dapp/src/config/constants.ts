f76f760ab676c4727b59c8eb7d1018b7387150a7
Mariano Aguero <mariano.aguero@gmail.com>
2021-08-03 01:06:48 -0300

    Add constant

--- examples/dapp/src/config/constants.ts
+++ examples/dapp/src/config/constants.ts
@@ -1,7 +1,7 @@
 export const INFURA_ID = process.env.REACT_APP_INFURA_ID
 export const DEFAULT_NETWORK_ID = Number(process.env.REACT_APP_DEFAULT_NETWORK_ID || 4)
 export const AVAILABLE_NETWORKS = [1, 137, 80001, 4, 43114, 43113]
-
+export const NETWORK_EXPLORER = process.env.REACT_APP_DEFAULT_NETWORK_EXPLORER || ''
 export const NETWORKS: { [key: number]: string } = {
   1: 'Mainnet',
   4: 'Rinkeby',
